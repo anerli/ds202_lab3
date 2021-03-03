7286ed442cf1e103583cdfb822e06240002f2233
anerli <anders.erik.lie@gmail.com>
2021-03-03 17:24:10 -0600

    3

--- anders.R
+++ anders.R
@@ -1,6 +1,7 @@
 # Libraries
-library(ggplot2)
-library(dpylr)
+#library(ggplot2)
+#library(dpylr)
+library(tidyverse)
 
 # Requires install.packages('readxl')
 dat <- readxl::read_xls('GSS.xls')
@@ -99,6 +100,70 @@ better_educated %>%
 '
 
 # 3
+#happiness_vals = c(2,1,0,NA)
+#names(happiness_vals) = c('Very happy', 'Pretty happy', 'Not too happy', "Don't know")
 
+"
+get_hap_val <- function(hap_str){
+  if (hap_str == 'Very happy') return(2);
+  if (hap_str == 'Pretty happy') return(1);
+  if (hap_str == 'Not too happy') return(0);
+  
+  # For Don't know / No answer / NA
+  return(NA);
+}
+"
+
+dat <- dat %>%
+  mutate(hap_val=
+          ifelse(happiness == 'Very happy', 2,
+          ifelse(happiness == 'Pretty happy', 1,
+          ifelse(happiness == 'Not too happy', 0, NA
+          )))
+        )
+  #mutate(hap_val=get_hap_val(happiness))
+
+dat %>%
+  group_by(marital_status) %>%
+  summarize(avg_happiness_val=mean(hap_val, na.rm=TRUE)) %>%
+  arrange(desc(avg_happiness_val))
+
+dat <- dat %>%
+  mutate(hap_bool=
+          ifelse(hap_val >= 1, TRUE,
+          ifelse(hap_val == 0, FALSE, NA
+          ))
+        )
+
+
+#dat <- dat %>%
+#  mutate(hap_val=switch(
+#    'Very happy'=2,
+#    'Pretty happy'=1,
+#    'Not too happy'=0,
+#    "Don't know"=NA,
+#    'No answer'=NA
+#  ))
+
+#dat <- dat %>%
+#  mutate(happiness)
+
+#dat$happiness <- factor(dat$happiness 
+
+str(dat)
+
+#%>% count(marital_status) %>% mutate(perc=n/nrow(marital_status)),
+
+#count_sorted_order <- dat %>%
+#  count(marital_status) %>%
+#  .$marital_status
+
+#ggplot(dat, aes(x=reorder(marital_status, count_sorted_order), fill=hap_bool)) + geom_bar(position='dodge')
+
+#dat$marital_status <- factor(dat$marital_status)
+dat$marital_status <- fct_infreq(dat$marital_status)
+#reorder(dat$marital_status, dat$martial_status, FUN=length)
+
+ggplot(dat, aes(x=marital_status, fill=hap_bool)) + geom_bar(position='dodge')
 
 
